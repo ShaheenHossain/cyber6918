bbd30e58a090a31870e8323945949de4109bc51a
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-09-13 01:28:36 +0600

    Update res_partner.py

--- eschool/models/res_partner.py
+++ eschool/models/res_partner.py
@@ -4,15 +4,15 @@ from odoo import models, fields, api
 
 class examtabulationwizard(models.Model):
     _inherit = 'res.partner'
-    name_b=fields.Char("নামের প্রথম অংশ")
+    name_b=fields.Char("নাম")
     # middle_name=fields.Char('Middle Name')
     # last_name=fields.Char('Last Name')
     # middle_name_b=fields.Char("নামের মধ্যাংশ")
     # last_name_b=fields.Char("নামের শেষাংশ")
     nid_no=fields.Char('NID No')
     car_no=fields.Char('Car No')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')],
-                              string='Gender', required=True, default='male', track_visibility='onchange')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')],
+                              string='Gender', track_visibility='onchange')
 
     _sql_constraints = [
         ('nid_no', 'unique(nid_no)', "NID number must be unique!"),
